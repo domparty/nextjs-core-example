66fa39008922e6599bf302b64fb4516b50f012d8
Glenn de Haan <glenn@dehaan.cloud>
2020-03-31 23:51:16 +0200

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+/**
+ * Import base packages
+ */
+const App = require('@domparty/nextjs-core');
+
+/**
+ * Create new Next.JS Core
+ */
+const core = new App(__dirname, process.cwd());
+
+/**
+ * Init the core
+ */
+core.init();
